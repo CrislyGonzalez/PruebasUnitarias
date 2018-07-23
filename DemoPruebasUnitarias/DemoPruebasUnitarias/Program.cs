199f0e37f0ed712f2cce1f408ce826b01b25257d
Crisly González Sánchez <gonzalezcrisly@gmail.com>
2018-07-23 09:55:28 -0600

    PruebasUnitariasDemo

---
+++ DemoPruebasUnitarias/DemoPruebasUnitarias/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPruebasUnitarias
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int Suma(int sumadoUno, int sumadoDos)
+        {
+            return sumadoUno + sumadoDos;
+        }
+    }
+}
---
+++ DemoPruebasUnitarias/PruebasUnitariasTest/Sumatest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoPruebasUnitarias;
+
+namespace PruebasUnitariasTest
+{
+    [TestClass]
+    public class SumaTest
+    {
+        [TestMethod]
+        public void SumaValoresValidos()
+        {
+            //arrange o planteamiento
+            const int sumadoUno = 3;
+            const int sumadoDos = 5;
+            const int esperado = 8;
+
+            //Act o Prueba
+            var actual = Program.Suma(sumadoUno, sumadoDos);
+
+       
+            //Asset
+            Assert.AreEqual(esperado, actual);
+
+
+        }
+    }
+}
